bcd037bf80ea05e29dfe701eb4a56a3f88aebed0
SBrianZ <stevenbrianzavala@gmail.com>
2026-02-19 15:43:38 -0800

    Office Hours Notes
    > Pushed some notes and random comments and pieces of code here and there from the 2/19 office hours.
    >> Went over pipeline for reading a .csv without storing the file in source control.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # covid-19-vaccine-adverse-reactions
+
+## Quickstart
+```bash
+python -m venv .venv
+source .venv/bin/activate  # (Windows: .venv\Scripts\activate)
+pip install -r requirements.txt
+streamlit run app.py
+```
+
+## Submission
+- **Streamlit deployed link:** asdf.
+- **GitHub repo link:** asdf.
+- **Description:** asdf.
--- src/data.py
+++ src/data.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 #the rest is up to you
+# df = df.read_csv(path)
+# return df
+# check to see if data folder exists. If not create it,
+# check to see if clean data.csv exists.
+    # if does, so we df = pd.read_csv("data/cean_data.csv")
+# if doesn't exist, we check to see if og data files exist
+    # if they don't then wedownload from kagglehub, save them to data folder
+# process / clean / trasnform data
+# write it to  data/clean_data.csv
+# df = pd.read_csv("data/clean_Data.csv")
