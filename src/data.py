dc4af54bcd0e4ed8bf047d3ac440c923d523ae84
ekmys13 <ekmys@proton.me>
2026-02-27 18:11:08 -0800

    drafted merge_dfs function; includes filtering out non-Pfizer and non-Moderna rows

--- src/data.py
+++ src/data.py
@@ -1,44 +1,27 @@
 ## Import Libraries
 import pandas as pd
 import streamlit as st
+from pandas import DataFrame
 
-## Import cleaned dataframes
-from cleaning_VAERS import df_VAERS_filtered
-from cleaning_vax import df_vax
-from cleaning_symptoms import df_symptoms_reshaped
+# required cache decorator - needed for streamlit to function
+@st.cache_data(show_spinner=False)
 
-@st.cache_data(show_spinner=False) # this is a decorator --> really need it for streamlit to function effectively
-def get_dfs(df1, df2, df3) -> pd.DataFrame:
-    df_VAERS_cl = df1
-    df_vax_cl = df2
-    df_symptoms_cl = df3
-    return df_VAERS_cl, df_vax_cl, df_symptoms_cl
+'''Merge cleaned dataframes from cleaning .py scripts into one dataframe.'''
+def merge_dfs() -> pd.DataFrame:
+    # import dataframes from cleaned scripts
+    from cleaning_VAERS import df_VAERS_filtered
+    from cleaning_vax import df_vax
+    from cleaning_symptoms import df_symptoms_reshaped
 
-def merge_dfs(df1, df2, df3) -> pd.DataFrame:
+    # merge df_symptoms_cl and df_VAERS_cl
+    df_merged_symptoms_VAERS: DataFrame = pd.merge(df_symptoms_reshaped, df_VAERS_filtered, how='left', on='VAERS_ID')
 
+    # merge df_vax_cl with previous merge
+    df_merged_all = pd.merge(df_merged_symptoms_VAERS, df_vax, how='left', on='VAERS_ID')
 
+    # drop non-Pfizer and non-Moderna rows
+    idx_to_drop = df_merged_all[
+        (df_merged_all['VAX_MANU'] != 'PFIZER\\BIONTECH') & (df_merged_all['VAX_MANU'] != 'MODERNA')].index
+    df = df_merged_all.drop(idx_to_drop)
 
-
-# from Lab06 -- using to adapt
-def load_data(path: str) -> pd.DataFrame:
-    '''Loading a small CSV and caching it so the app stays responsive.'''
-    df = pd.read_csv(path) # create dataframe
-    df['created_date'] = pd.to_datetime(df['created_date'], errors='coerce')
     return df
-
-'''We might create some additional functions/methods in here for data transformations'''
-'''Data transformations would take place in THIS file (but cleaning would take place elsewhere, seems like??)'''
-
-
-
-#the rest is up to you
-# df = df.read_csv(path)
-# return df
-# check to see if data folder exists. If not create it,
-# check to see if clean data.csv exists.
-    # if does, so we df = pd.read_csv("data/cean_data.csv")
-# if doesn't exist, we check to see if og data files exist
-    # if they don't then wedownload from kagglehub, save them to data folder
-# process / clean / trasnform data
-# write it to  data/clean_data.csv
-# df = pd.read_csv("data/clean_Data.csv")
