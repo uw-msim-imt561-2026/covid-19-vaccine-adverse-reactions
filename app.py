b6fa4528eacb973dc748beddec34fa975f216439
SBrianZ <stevenbrianzavala@gmail.com>
2026-02-28 04:30:29 -0800

    Pushing To Fix Streamlit
    > Trying to figure out issues with getting our dashboard deployed via Streamlit.

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ import subprocess
 import sys
 
 ## Grabs functions & scripts from other .py's
-from src import cleaning_VAERS
-from src import cleaning_vax
-from src import cleaning_symptoms
+#from src import cleaning_VAERS
+#from src import cleaning_vax
+#from src import cleaning_symptoms
 from src.data import merge_dfs
 from src.layouts import header_metrics, body_layout_tabs
 
 ## Run cleaning scripts
 # TODO - verify that the subprocess.run code below works for running cleaning scripts
-subprocess.run([f"{sys.executable}", "cleaning_VAERS.py"])
-subprocess.run([f"{sys.executable}", "cleaning_vax.py"])
-subprocess.run([f"{sys.executable}", "cleaning_symptoms.py"])
+#subprocess.run([f"{sys.executable}", "cleaning_VAERS.py"])
+#subprocess.run([f"{sys.executable}", "cleaning_vax.py"])
+#subprocess.run([f"{sys.executable}", "cleaning_symptoms.py"])
 # Streamlit subprocess documentation: https://docs.streamlit.io/knowledge-base/deploy/invoking-python-subprocess-deployed-streamlit-app
 
 def main() -> None:
@@ -24,16 +24,17 @@ def main() -> None:
 
     # -------------------------
     ## Header (sidebar by default)
-    st.title("COVID-19 VAERS Dashboard")
-    st.caption("TEAM VAERS: AJ Amrous, Em Stelter, and S Brian Zavala")
+    #st.title("COVID-19 VAERS Dashboard") <- replaced with st.image
+    st.image("visualizations/logos/Header_VAERS.png",width='content',clamp=True) # prolly want to convert this to an .svg at some point
+    st.caption("An interactive data visualization dashboard for adverse vaccine events and reactions.",text_alignment="center")
     # -------------------------
 
     ## Load Cached Data
-    df = merge_dfs()
+    #df = merge_dfs()
     # sanity check
-    print(df.head(5))
-    st.write("Row count: ", len(df))
-    st.dataframe(df.head(5))
+    #print(df.head(5))
+    #st.write("Row count: ", len(df))
+    #st.dataframe(df.head(5))
 
     # -------------------------
     ## Filters (sidebar by default)
