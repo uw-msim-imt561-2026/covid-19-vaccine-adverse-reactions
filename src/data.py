2dd5e1a2ecfaa8310dd5c625ea0ed3fc28397085
ekmys13 <ekmys@proton.me>
2026-02-27 17:34:11 -0800

    data.py drafting

--- src/data.py
+++ src/data.py
@@ -1,4 +1,35 @@
+## Import Libraries
+import pandas as pd
 import streamlit as st
+
+## Import cleaned dataframes
+from cleaning_VAERS import df_VAERS_filtered
+from cleaning_vax import df_vax
+from cleaning_symptoms import df_symptoms_reshaped
+
+@st.cache_data(show_spinner=False) # this is a decorator --> really need it for streamlit to function effectively
+def get_dfs(df1, df2, df3) -> pd.DataFrame:
+    df_VAERS_cl = df1
+    df_vax_cl = df2
+    df_symptoms_cl = df3
+    return df_VAERS_cl, df_vax_cl, df_symptoms_cl
+
+def merge_dfs(df1, df2, df3) -> pd.DataFrame:
+
+
+
+
+# from Lab06 -- using to adapt
+def load_data(path: str) -> pd.DataFrame:
+    '''Loading a small CSV and caching it so the app stays responsive.'''
+    df = pd.read_csv(path) # create dataframe
+    df['created_date'] = pd.to_datetime(df['created_date'], errors='coerce')
+    return df
+
+'''We might create some additional functions/methods in here for data transformations'''
+'''Data transformations would take place in THIS file (but cleaning would take place elsewhere, seems like??)'''
+
+
 
 #the rest is up to you
 # df = df.read_csv(path)
