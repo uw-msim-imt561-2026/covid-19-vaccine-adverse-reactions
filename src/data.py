28d3a366ebceac9d6c3c8391a115328569c5b11f
SBrianZ <stevenbrianzavala@gmail.com>
2026-03-01 15:08:40 -0800

    Filters Update & Data Fix
    > Got Date of Report Slider to function
    > Made adjustments to data.py script to reconvert RECVDATE back into datetime so streamlit could read it.
    > Pretty'ing up filters section

--- src/data.py
+++ src/data.py
@@ -6,4 +6,5 @@ import streamlit as st
 @st.cache_data(show_spinner=False)
 def load_data() -> pd.DataFrame:
     df = pd.read_csv('data/west_states_filtered.csv')
+    df["RECVDATE"] = pd.to_datetime(df["RECVDATE"])
     return df
