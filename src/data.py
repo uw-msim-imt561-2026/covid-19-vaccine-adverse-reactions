d031a35f02f5932952a2029a08cfa115907d8a2b
ekmys13 <ekmys@proton.me>
2026-03-01 17:14:55 -0800

    Added KPI functions to data.py

--- src/data.py
+++ src/data.py
@@ -11,3 +11,28 @@ def load_data() -> pd.DataFrame:
     df['SEX'] = df['SEX'].replace('M', 'Male')
     df['SEX'] = df['SEX'].replace('U', 'Unknown or Undisclosed')
     return df
+
+## Calculate KPI metrics
+def get_total_events_kpi(df: pd.DataFrame) -> str:
+    """Calculates the total sum of VAERS reports in a given span of time (already filtered in the dataframe arg)."""
+    df_kpi_grouped = (df.groupby(by=["MONTH_YEAR"]).agg(report_count=("VAERS_ID", "count"))).reset_index()
+    total_reports = df_kpi_grouped['report_count'].sum()
+    total_reports_str = str(total_reports)
+    return total_reports_str
+
+def get_total_hosp_kpi(df: pd.DataFrame) -> str:
+    """Calculates the total sum of VAERS reports in a given span of time (already filtered in the dataframe arg), in which the patient was hospitalized."""
+    df_hosp = df[df['HOSPITAL'] == 'Y']
+    df_kpi_hosp = (df_hosp.groupby(by=["MONTH_YEAR"]).agg(report_count=("VAERS_ID", "count"))).reset_index()
+    total_hosp = df_kpi_hosp['report_count'].sum()
+    total_hosp_str = str(total_hosp)
+    return total_hosp_str
+
+def get_total_died_kpi(df: pd.DataFrame) -> str:
+    """Calculates the total sum of VAERS reports in a given span of time (already filtered in the dataframe arg), in which the patient died."""
+    df_died = df[df['DIED'] == 'Y']
+    df_kpi_died = (df_died.groupby(by=["MONTH_YEAR"]).agg(report_count=("VAERS_ID", "count"))).reset_index()
+    total_died = df_kpi_died['report_count'].sum()
+    total_died_str = str(total_died)
+    return total_died_str
+
